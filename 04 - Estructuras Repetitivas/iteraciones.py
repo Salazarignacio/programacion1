e39aa967a39b15584d906f68cd51378b56acaa23
ignacio salazar <ignaciosalazar986@gmail.com>
2025-04-06 21:00:44 -0300

    ejercicios

---
+++ 04 - Estructuras Repetitivas/iteraciones.py
@@ -0,0 +1,17 @@
+""" Desarrollar un algoritmo que permita ingresar una cantidad de personas. La computadora debe
+pedir la edad de cada una y finalmente mostrar el porcentaje de personas que es mayor de edad """
+
+
+
+mayores = 0.0
+porcentaje = 0
+personas = 6
+
+for cont in range(personas):
+    edadPersona = int(input("Ingrese su edad"))
+    if edadPersona > 17:
+        mayores += 1
+porcentaje = 100 / personas * mayores
+print(porcentaje)
+""" 1) porcentaje = 100 / cont * mayores """        
+""" 2)  porcentaje = porcentaje """
